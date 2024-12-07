9a6967ece722fef63567126d75a457437cbd01af
ryotakurashiki <kura.hutak0bu@gmail.com>
2024-12-07 15:15:38 +0700

    予算を超えそうかどうかの予測を表示

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import type { ResponseHomeData } from '../../api_types/home_data';
 import ExpenseList from './components/ExpenseList/ExpenseList';
+import { current_days, this_month_days } from './functions/date';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,12 +22,19 @@ function App() {
     alert("Button clicked!");
   };
 
+  function calc_budget_rate(budget_amount: number, expense_total_amount: number): number {
+    const estimated_expense_total_amount = expense_total_amount / current_days() * this_month_days();
+    return Math.floor(estimated_expense_total_amount/budget_amount*100);
+  }
+
   const bugetsResultItems = (data?.budget_with_results || []).map(budget_with_result => {
+    const rate = calc_budget_rate(budget_with_result.budget_amount, budget_with_result.expense_total_amount);
     return (
       <div key={budget_with_result.budget_id}>
         <div>{ budget_with_result.large_category_name }</div>
         <div>予算{ budget_with_result.currency_symbol }{ budget_with_result.budget_amount }</div>
         <div>実績{ budget_with_result.currency_symbol }{ budget_with_result.expense_total_amount }</div>
+        {rate > 100 ? (<div>！このペースだと予算を{budget_with_result.currency_symbol}{budget_with_result.budget_amount*(rate-100)/100}オーバーします</div>) : (<div></div>) }
       </div>
     )
   });
--- frontend/src/functions/date.ts
+++ frontend/src/functions/date.ts
@@ -3,3 +3,12 @@ import dayjs from 'dayjs';
 export function get_string_from_date(date: Date, format?: string): string {
   return dayjs(date).format(format);
 }
+
+export function current_days(): number {
+  return Number(dayjs().format("D"));
+}
+
+export function this_month_days(): number {
+  return Number(dayjs().endOf('month').format("D"));
+}
+
